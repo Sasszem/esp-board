a5503855796c21a4191493179ac187a23a02a3c3
Sasszem <barath.laci10@gmail.com>
2019-02-13 23:55:53 +0100

    Add keyboard matrix

---
+++ libs/matrix.hpp
@@ -0,0 +1,12 @@
+
+class Matrix
+{
+    private:
+       int d[3];
+       ShiftRegister *reg;
+       int row;
+    public:
+       void update();
+       Matrix(int d1, int d2, int d3, ShiftRegister *r);
+       bool state[4][3];
+};
